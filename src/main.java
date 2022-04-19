c1e046fd84a5b1aa0c0e94614ae5e39e19d0a946
Alberto <alberyoshi@gmail.com>
2022-04-20 01:41:12 +0200

    Clases creadas, atributos y sus correspondientes get y set.
    prueba de hola mundo

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
+import controlador.Utilidades;
+
+//Inicio de la applicación.
 public class main {
 
     public static void main(String[] args) {
-
+        Utilidades.imprimir("hola mundo");
     }
 }
