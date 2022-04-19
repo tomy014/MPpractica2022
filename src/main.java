62a0e38e8cb338de6cdd266087e2045cac4fea64
Alberto <alberyoshi@gmail.com>
2022-04-20 01:45:28 +0200

    prueba de rama

--- src/main.java
+++ src/main.java
@@ -4,6 +4,6 @@ import controlador.Utilidades;
 public class main {
 
     public static void main(String[] args) {
-        Utilidades.imprimir("hola mundo");
+        Utilidades.imprimir("hola mundo 2");
     }
 }
