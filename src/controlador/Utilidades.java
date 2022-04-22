67e355a1fd05bfc1e6bc93c3348430ec3ef0a0f1
Alberto <alberyoshi@gmail.com>
2022-04-22 16:28:06 +0200

    Creada nueva rama Métodos para pedir cadena y eneteros creados

--- src/controlador/Utilidades.java
+++ src/controlador/Utilidades.java
@@ -1,11 +1,31 @@
 package controlador;
 
-public class Utilidades {
+import java.util.Scanner;
+
+public abstract class Utilidades {
 
     public static void pause(int sec) throws InterruptedException {
         Thread.sleep(sec * 800);
     }
+
     public static void imprimir(String cadena){
         System.out.println(cadena);
+    }
+
+    public static void limpiarPantalla(){
+        for (int i = 0; i < 14; i++)
+            System.out.println();
+    }
+
+    public static String pedirCadena(String cadena){
+        imprimir(cadena);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+    public static int pedirEntero(String cadena){
+        imprimir(cadena);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 }
